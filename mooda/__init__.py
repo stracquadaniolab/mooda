dee1ba4d210aee7b78455945225dcba8ece9d5db
gstracqu <giovanni.stracquadanio@ed.ac.uk>
2019-07-07 15:26:55 +0100

    updated mooda/__init__

--- mooda/__init__.py
+++ mooda/__init__.py
@@ -3,6 +3,6 @@
 """Top-level package for MOODA."""
 
 __author__ = """Angelo Gaeta Giovanni Stracquadanio"""
-__email__ = 'your@email.org'
-__version__ = '3.0.0'
+__email__ = 'a.gaeta@sms.ed.ac.uk'
+__version__ = '0.0.1-dev'
 
