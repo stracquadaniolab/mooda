c893cbe830d8d34f09a4fe6ede8df8676f7840ea
Angelo Gaeta <a.gaeta@sms.ed.ac.uk>
2019-07-09 15:28:48 +0100

    Bump version: 0.1.1-dev → 0.2.0-dev

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = 'Angelo Gaeta Giovanni Stracquadanio'
 author = 'Angelo Gaeta Giovanni Stracquadanio'
 
 # The short X.Y version
-version = '0.1.1-dev'
+version = '0.2.0-dev'
 # The full version, including alpha/beta/rc tags
-release = '0.1.1-dev'
+release = '0.2.0-dev'
 
 
 # -- General configuration ---------------------------------------------------
