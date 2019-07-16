ff31fcefe01cdbaee03d8ca4044cb83c3236d712
Angelo Gaeta <a.gaeta@sms.ed.ac.uk>
2019-07-16 09:48:04 +0100

    Bump version: 0.2.0-dev → 0.3.0-dev

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = 'Angelo Gaeta Giovanni Stracquadanio'
 author = 'Angelo Gaeta Giovanni Stracquadanio'
 
 # The short X.Y version
-version = '0.2.0-dev'
+version = '0.3.0-dev'
 # The full version, including alpha/beta/rc tags
-release = '0.2.0-dev'
+release = '0.3.0-dev'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # package information
     name = 'mooda',
     packages = find_packages(),
-    version = '0.2.0-dev',
+    version = '0.3.0-dev',
     description = 'Multi-Objective Optimisation algorithm for sequence Design and Assembly',
     long_description = readme,
     license = 'MIT',
