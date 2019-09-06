b991ca1f6fe8489974010d14568e074c400988cd
gstracqu <giovanni.stracquadanio@ed.ac.uk>
2019-09-06 17:37:14 +0100

    Bump version: 0.6.3-dev → 0.7.0-dev

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = 'Angelo Gaeta Giovanni Stracquadanio'
 author = 'Angelo Gaeta Giovanni Stracquadanio'
 
 # The short X.Y version
-version = '0.6.3-dev'
+version = '0.7.0-dev'
 # The full version, including alpha/beta/rc tags
-release = '0.6.3-dev'
+release = '0.7.0-dev'
 
 
 # -- General configuration ---------------------------------------------------
