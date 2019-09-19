decc71dca9d7199af3875259fb8a2b917a38589d
Angelo <s1899269@ed.ac.uk>
2019-09-19 16:53:45 +0100

    Bump version: 0.5.1-dev → 0.6.0-dev

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = 'Angelo Gaeta Giovanni Stracquadanio'
 author = 'Angelo Gaeta Giovanni Stracquadanio'
 
 # The short X.Y version
-version = '0.5.1-dev'
+version = '0.6.0-dev'
 # The full version, including alpha/beta/rc tags
-release = '0.5.1-dev'
+release = '0.6.0-dev'
 
 
 # -- General configuration ---------------------------------------------------
--- mooda/__init__.py
+++ mooda/__init__.py
@@ -4,5 +4,5 @@
 
 __author__ = """Angelo Gaeta Giovanni Stracquadanio"""
 __email__ = 'a.gaeta@sms.ed.ac.uk'
-__version__ = '0.5.1-dev'
+__version__ = '0.6.0-dev'
 
