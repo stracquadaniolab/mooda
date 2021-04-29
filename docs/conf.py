c9db03db0adc652dca4898cbfee05109515b8a1f
Giovanni Stracquadanio <giovanni.stracquadanio@ed.ac.uk>
2021-04-29 12:56:13 +0100

    Bump version: 0.9.0 → 0.9.1

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = 'Angelo Gaeta Giovanni Stracquadanio'
 author = 'Angelo Gaeta Giovanni Stracquadanio'
 
 # The short X.Y version
-version = '0.9.0'
+version = '0.9.1'
 # The full version, including alpha/beta/rc tags
-release = '0.9.0'
+release = '0.9.1'
 
 
 # -- General configuration ---------------------------------------------------
